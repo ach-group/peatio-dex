8feb54687c4e2fbf5014d8db9bd59a9e1d1e864b
Jebatech-Crypto <51023977+Jebatech-Crypto@users.noreply.github.com>
2019-10-01 14:53:30 +0800

    Update deposit.rb

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -102,7 +102,7 @@ class Deposit < ActiveRecord::Base
   private
 
   def do
-    total_fee = self.staking_fee_account * amount
+    total_fee = self.staking * amount
     account.lock!.plus_funds amount, fee: total_fee, reason: Account::DEPOSIT, ref: self
 
     if self.staking
